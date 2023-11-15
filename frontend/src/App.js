74ca3197ff4f84f1b9efa1208df24965a71771bf
Minami ASATO <e215701@ie.u-ryukyu.ac.jp>
2023-11-15 13:16:53 +0900

    add(frontend): add History page and Recommend page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 //この行以降に、遷移するpageの読み込み文を宣言する。
-import Toppage from "./pages/Top";
-import PreviewUpload from "./pages/Upload";
+import Top from "./pages/Top";
+import Upload from "./pages/Upload";
+import Recommend from "./pages/Recommend";
+import History from "./pages/History";
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Toppage />} />
-          <Route path="/PreviewUpload" element={<PreviewUpload />} />
+          <Route path="/" element={<Top />} />
+          <Route path="/Upload" element={<Upload />} />
+          <Route path="/Recommend" element={<Recommend />} />
+          <Route path="/History" element={<History />} />
         </Routes>
       </BrowserRouter>
     );
