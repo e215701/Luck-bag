c513f3d9789335973fb3ae6ae980f8506c125a74
Matayoshi IBUKI <e215712@ie.u-ryukyu.ac.jp>
2023-11-18 13:58:33 +0900

    update(frontend): separate Upload page

--- frontend/src/pages/Recommend.js
+++ frontend/src/pages/Recommend.js
@@ -1,26 +1,130 @@
-import React from "react"; //Reactを読み込んでいる
-import { Link } from "react-router-dom"; // 追加 Linkタブを読み込む
-import "../css/style.css"; //CSSを適用したい場合はimportで
+import React from "react";
+import axios from "axios";
+import { useLocation, Link } from "react-router-dom";
 
-class SelectTags extends React.Component {
-  //必要になる関数の定義はrender(){}以前に
+class Recommend extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imageData: null,
+      image: null,
+      loading_image: false,
+      error_image: null,
+      loading: false,
+      error: null,
+      responseData: null,
+    };
+  }
 
-  //render(){}がページ描画部分関連
+  componentDidMount() {
+    const imageData = this.props.location.state?.image;
+    if (imageData) {
+      this.setState({ imageData: imageData }, () => {
+        this.fetchData(imageData);
+      });
+    }
+  }
+
+  async query(data) {
+    this.setState({ loading_image: true, error_image: null });
+
+    try {
+      const response = await axios.post(
+        "https://api-inference.huggingface.co/models/goofyai/Leonardo_Ai_Style_Illustration",
+        data,
+        {
+          //headers: { Authorization: "Bearer {API_TOKEN}" },
+          responseType: "blob",
+        }
+      );
+
+      const blob = URL.createObjectURL(response.data);
+      this.setState({ image: blob, loading_image: false });
+      console.log("成功");
+    } catch (error) {
+      // エラーハンドリングを行うことをお勧めします
+      console.error("エラー:", error);
+      this.setState({
+        loading_image: false,
+        error_image: "画像を生成できませんでした。",
+      });
+      throw error;
+    }
+  }
+
+  async fetchData(file) {
+    this.setState({ loading: true, error: null });
+
+    try {
+      // Base64エンコードされたデータをBlobに変換
+      const response = await fetch(file);
+      const blob = await response.blob();
+
+      // BlobをPNGファイルに変換
+      const pngFile = new File([blob], "image.png", { type: "image/png" });
+
+      // FormDataオブジェクトを作成し、PNGファイルを追加
+      // const formData = new FormData();
+      // formData.append("file", pngFile);
+
+      // APIリクエスト
+      const responseAPI = await axios.post(
+        "https://api-inference.huggingface.co/models/valentinafeve/yolos-fashionpedia",
+        pngFile,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      console.log(JSON.stringify(responseAPI.data));
+      //this.setState({ responseData: response.data, loading: false });
+      const labels = responseAPI.data.map((item) => item.label);
+      const splitlabels = labels.flatMap((label) =>
+        label.split(",").map((s) => s.trim())
+      );
+
+      //console.log(JSON.stringify(labels));
+      console.log(JSON.stringify(splitlabels));
+      this.setState({ responseData: splitlabels, loading: false });
+
+      // query関数を呼び出す例
+      const infomation = JSON.stringify(splitlabels);
+      this.query({ inputs: infomation });
+    } catch (error) {
+      console.error("API通信エラー:", error);
+      this.setState({
+        loading: false,
+        error: "データを取得できませんでした。",
+      });
+    }
+  }
+
   render() {
-    //ページ描画で必要な変数はここで定義
-    // 例: Text = "HelloWorld"
+    const { imageData, image, loading, error } = this.state;
+
     return (
-      //ここにページ描画部分
-      <body>
-        //ここにページ描画部分
-        <div className="App">
-          <h1>コーディネートを紹介するページ</h1>
-        </div>
-        //画面遷移はLinkタグを用いる。{}の中は、遷移先のパス
+      <div className="App">
+        <h1>コーディネートを紹介するページ</h1>
+        {loading && <p>読み込み中...</p>}
+        {error && <p>エラー: {error}</p>}
+        {image && (
+          <div>
+            <h2>生成された画像</h2>
+            <img src={image} alt="Generated" />
+          </div>
+        )}
+        {imageData && (
+          <div>
+            <h2>アップロードした画像</h2>
+            <img src={imageData} alt="Uploaded" />
+          </div>
+        )}
         <Link to={`/`}>TOP</Link>
-      </body>
+      </div>
     );
   }
 }
 
-export default SelectTags; //ここで実際にPage表示（これがないと表示されない）
+export default (props) => <Recommend {...props} location={useLocation()} />;
--- frontend/src/pages/Upload.js
+++ frontend/src/pages/Upload.js
@@ -1,150 +1,49 @@
-import React from "react";
-import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-class PreviewUpload extends React.Component {
-  constructor(props) {
-    super(props);
+const Upload = () => {
+  const [imageData, setImageData] = useState(null);
+  const navigate = useNavigate();
 
-    this.state = {
-      imageData: null,
-      logs: null,
-      loading: false,
-      error: null,
-      loading_image: false,
-      image: null,
-      error_image: null,
-    };
-  }
-
-  async query(data) {
-    this.setState({ loading_image: true, error_image: null });
-
-    try {
-      const response = await axios.post(
-        "https://api-inference.huggingface.co/models/goofyai/Leonardo_Ai_Style_Illustration",
-        data,
-        {
-          //headers: { Authorization: "Bearer {API_TOKEN}" },
-          responseType: "blob",
-        }
-      );
-
-      const blob = URL.createObjectURL(response.data);
-      this.setState({ image: blob, loading_image: false });
-      console.log("成功");
-    } catch (error) {
-      // エラーハンドリングを行うことをお勧めします
-      console.error("エラー:", error);
-      this.setState({
-        loading_image: false,
-        error_image: "画像を生成できませんでした。",
-      });
-      throw error;
-    }
-  }
-
-  async fetchData(file) {
-    this.setState({ loading: true, error: null });
-
-    try {
-      const fileData = file;
-
-      const response = await axios.post(
-        "https://api-inference.huggingface.co/models/valentinafeve/yolos-fashionpedia",
-        fileData,
-        {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        }
-      );
-
-      console.log(JSON.stringify(response.data));
-      //this.setState({ responseData: response.data, loading: false });
-      const labels = response.data.map((item) => item.label);
-      const splitlabels = labels.flatMap((label) =>
-        label.split(",").map((s) => s.trim())
-      );
-
-      //console.log(JSON.stringify(labels));
-      console.log(JSON.stringify(splitlabels));
-      this.setState({ responseData: splitlabels, loading: false });
-
-      // query関数を呼び出す例
-      const infomation = JSON.stringify(splitlabels);
-      this.query({ inputs: infomation });
-    } catch (error) {
-      console.error("API通信エラー:", error);
-      this.setState({
-        loading: false,
-        error: "データを取得できませんでした。",
-      });
-    }
-  }
-
-  onFileChange(e) {
+  const onFileChange = (e) => {
     const files = e.target.files;
 
     if (files.length > 0) {
       const file = files[0];
       const reader = new FileReader();
       reader.onload = (e) => {
-        this.setState({ imageData: e.target.result }, () => {
-          // 画像が読み込まれたらAPIを呼び出す
-          this.fetchData(file);
-        });
+        setImageData(e.target.result);
       };
       reader.readAsDataURL(file);
     } else {
-      this.setState({ imageData: null });
+      setImageData(null);
     }
-  }
+  };
 
-  render() {
-    const {
-      responseData,
-      loading,
-      imageData,
-      error,
-      loading_image,
-      error_image,
-      image,
-    } = this.state;
+  const goToRecommend = () => {
+    navigate("/recommend", { state: { image: imageData } });
+  };
 
-    return (
-      <div>
-        <input
-          id="upload"
-          type="file"
-          name="image"
-          accept="image/*"
-          capture="camera"
-          onChange={(e) => {
-            this.onFileChange(e);
-          }}
-        />
-        {loading ? (
-          <p>データを取得中...</p>
-        ) : error ? (
-          <p>{error}</p>
-        ) : responseData ? (
-          <div>
-            <h2>画像データ:</h2>
-            <img src={imageData} alt="Image" width="200" height="60" />
-            <h2>APIからのデータ:</h2>
-            <pre>{JSON.stringify(responseData, null, 2)}</pre>
-            {loading_image ? (
-              <p>画像を生成中...</p>
-            ) : error_image ? (
-              <p>{error_image}</p>
-            ) : image ? (
-              <img src={image} alt="Image" width="200" height="60" />
-            ) : null}
-          </div>
-        ) : null}
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <input
+        id="upload"
+        type="file"
+        name="image"
+        accept="image/*"
+        capture="camera"
+        onChange={onFileChange}
+      />
+      {imageData ? (
+        <div>
+          <h2>画像</h2>
+          <img src={imageData} alt="Uploaded" width="200" height="60" />
+          <br />
+          <button onClick={goToRecommend}>Recommend Pageへ</button>
+        </div>
+      ) : null}
+    </div>
+  );
+};
 
-export default PreviewUpload;
+export default Upload;
