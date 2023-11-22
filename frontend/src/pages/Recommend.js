b6e942d8ee510c658fb3f6e738133b8f7ef0b320
mii <93558693+e215701@users.noreply.github.com>
2023-11-22 16:46:04 +0900

    Revert "Ibuki"

--- frontend/src/pages/Recommend.js
+++ frontend/src/pages/Recommend.js
@@ -3,15 +3,6 @@ import axios from "axios";
 import { useLocation, Link } from "react-router-dom";
 
 class Recommend extends React.Component {
-  handleClick = () => {
-    // ボタンがクリックされた時の処理をここに記述
-    console.log("ボタンがクリックされました！");
-    // ここで関数を呼び出すなどの処理を記述する
-    this.setState({ image: null });
-    const imageData = this.state.imageData;
-    this.fetchData(imageData);
-  };
-
   constructor(props) {
     super(props);
     this.state = {
@@ -35,12 +26,7 @@ class Recommend extends React.Component {
   }
 
   async query(data) {
-    if (this.state.loading_image) {
-      // すでにAPI呼び出しが行われている場合は何もしない
-      return;
-    }
-
-    this.setState({ loading_image: true, error_image: null, image: null });
+    this.setState({ loading_image: true, error_image: null });
 
     try {
       const response = await axios.post(
@@ -67,10 +53,6 @@ class Recommend extends React.Component {
   }
 
   async fetchData(file) {
-    if (this.state.loading) {
-      // すでにAPI呼び出しが行われている場合は何もしない
-      return;
-    }
     this.setState({ loading: true, error: null });
 
     try {
@@ -80,6 +62,10 @@ class Recommend extends React.Component {
 
       // BlobをPNGファイルに変換
       const pngFile = new File([blob], "image.png", { type: "image/png" });
+
+      // FormDataオブジェクトを作成し、PNGファイルを追加
+      // const formData = new FormData();
+      // formData.append("file", pngFile);
 
       // APIリクエスト
       const responseAPI = await axios.post(
@@ -93,11 +79,13 @@ class Recommend extends React.Component {
       );
 
       console.log(JSON.stringify(responseAPI.data));
+      //this.setState({ responseData: response.data, loading: false });
       const labels = responseAPI.data.map((item) => item.label);
       const splitlabels = labels.flatMap((label) =>
         label.split(",").map((s) => s.trim())
       );
 
+      //console.log(JSON.stringify(labels));
       console.log(JSON.stringify(splitlabels));
       this.setState({ responseData: splitlabels, loading: false });
 
@@ -114,28 +102,20 @@ class Recommend extends React.Component {
   }
 
   render() {
-    const { imageData, image, loading, error, loading_image, error_image } =
-      this.state;
+    const { imageData, image, loading, error } = this.state;
 
     return (
       <div className="App">
         <h1>コーディネートを紹介するページ</h1>
-        {loading && <p>タグを取得中...</p>}
+        {loading && <p>読み込み中...</p>}
         {error && <p>エラー: {error}</p>}
-        {loading_image && <p>画像を生成中...</p>}
-        {error_image && <p>エラー: {error}</p>}
         {image && (
           <div>
             <h2>生成された画像</h2>
             <img src={image} alt="Generated" width="200" height="60" />
-            <div>
-              <a href={image} download>
-                <button style={{ padding: "5px" }}>画像をダウンロード</button>
-              </a>
-            </div>
-            <div>
-              <button onClick={this.handleClick}>再生成する</button>
-            </div>
+            <a href={image} download>
+              <button style={{ padding: "5px" }}>画像をダウンロード</button>
+            </a>
           </div>
         )}
         {imageData && (
