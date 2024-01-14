4da4055461f53c361f03a1f946cec2cc614eb5a3
Yuuki Takusi <e215737@ie.u-ryukyu.ac.j>
2024-01-14 17:33:09 +0900

    update(frontend): add gender select button

--- frontend/src/pages/Upload.js
+++ frontend/src/pages/Upload.js
@@ -8,10 +8,12 @@ const Upload = () => {
   const [imageData, setImageData] = useState(null);
   const [screenHeight, setScreenHeight] = useState(0);
   const [screenWidth, setScreenWidth] = useState(0);
+  const [gender, setGender] = useState("")
+  const [genderImage, setGenderImage] = useState({male: './images/gender_male_image.png', female: './images/gender_female_image.png', anything: './images/gender_anything_image.png'});
 
   useEffect(() => {
     if (imageData) {
-        goToRecommend();
+        // goToRecommend();
       }
     
 
@@ -51,51 +53,25 @@ const Upload = () => {
   };
 
   const goToRecommend = () => {
-    navigate("/Recommend", { state: { image: imageData } });
+    navigate("/Recommend", { state: { image: imageData, gender: gender } });
   };
 
+  const selectGender = (selectedGender) => {
+    setGender(selectedGender);
+    setGenderImage({
+      ...genderImage,
+      [selectedGender]: selectedGender === 'male' ? './images/gender_anything_image.png' : selectedGender === 'female' ? './images/gender_anything_image.png' : './images/gender_male_image.png'
+    });
+};
+
   return (
-    // <div className="upload-upload">
-    //   <div className="header">LuckBag</div>
-    //   <div className="upload-container">
-    //     <label
-    //       htmlFor="upload"
-    //       // style={{
-    //       //   alignItems: "center",
-    //       //   outline: "2px solid blue",
-    //       // }}
-    //     >
-        //   <div className="upload-picture-container">
-        //     <img className="upload-picture" alt="" src={imageData}></img>
-        //     <input
-        //       id="upload"
-        //       type="file"
-        //       name="image"
-        //       accept="image/*"
-        //       onChange={onFileChange}
-        //       style={{ display: "none" }}
-        //     />
-        //   </div>
-    //     </label>
-
-    //     <button className="upload-button" onClick={goToRecommend}>
-    //       Coordinate
-    //     </button>
-    //   </div>
-
-    //   {/* {imageData && (
-    //     <div>
-    //       <img src={imageData} alt="Uploaded" width="200" height="60" />
-    //     </div>
-    //   )} */}
-    // </div>
     <div id="upload-page">
         <div id="wrapper">
           <div id="content" className="fade-in">
             {/* <div className="header">LuckBag</div> */}
             <header class="header">
               <div class="navtext-container">
-                <div class="navtext">company</div>
+                <div class="navtext">Luck Bag</div>
               </div>
               <input type="checkbox" class="menu-btn" id="menu-btn" />
               <label for="menu-btn" class="menu-icon">
@@ -116,69 +92,122 @@ const Upload = () => {
                 </li>
               </ul>
             </header>
-
+            
             <div className="upload-page"
                 style={{
                   width: `${screenWidth}px`,
+                  height: `${screenHeight}px`,
                 }}
             >
 
-            <img
-                    className="upload-image"
-                    src="./images/image-12_1.jpeg"
-                    alt="image1"
-                    style={{
-                      height: `${screenHeight}px`,
-                      width: `${screenWidth}px`,
-                    }}
-                  />
+            <p>&thinsp;</p>
+            <p>&thinsp;</p>
+            <div className="upload-item-headline">
+            <p>あなたの気になっている</p>
+            <p>性別を選んでください</p>
+            </div>
+            <div className="gender-item-container">
+            <label className="gender-label">
+                <input
+                type="radio"
+                name="gender"
+                value="male"
+                checked={gender === 'male'}
+                onChange={() => selectGender('male')}
+                className="gender-input"
+                />
+                <span className="gender-custom-radio"></span>
+                Male
+            </label>
 
-              <div className="upload-item-container">
+            <label className="gender-label">
+                <input
+                type="radio"
+                name="gender"
+                value="female"
+                checked={gender === 'female'}
+                onChange={() => selectGender('female')}
+                className="gender-input"
+                />
+                <span className="gender-custom-radio"></span>
+                Female
+            </label>
+
+            <label className="gender-label">
+                <input
+                type="radio"
+                name="gender"
+                value="anything"
+                checked={gender === 'anything'}
+                onChange={() => selectGender('anything')}
+                className="gender-input"
+                />
+                <span className="gender-custom-radio"></span>
+                Anything
+            </label>
+            </div>
+
+                
+            <div className="upload-item-container">
                 <div className="upload-item-headline">写真のポイント</div>
                 <div className="upload-item-text-center">
-                  服の形がはっきりとわかる写真にしましょう。
+                    服の形がはっきりとわかる写真にしましょう。
                 </div>
-              </div>
-              <div className="upload-item-container">
+            </div>
+            <div className="upload-item-container">
                 <div className="upload-item-text-center">
-                  <p>写真に複数の服が写っていると、</p>
-                  <p>うまくいかない場合があります。</p>                
+                    <p>写真に複数の服が写っていると、</p>
+                    <p>うまくいかない場合があります。</p>                
                 </div>
-                <div className="upload-item-container">
-                {/* <button className="upload-button" onClick={goToRecommend}>
-                    写真をアップロードする
-                </button> */}
+            </div> 
+                    
+                    {/* <button className="upload-button" onClick={goToRecommend}>
+                        写真をアップロードする
+                    </button> */}
 
-                <div className="upload-picture-container">
-                    {/* <img className="upload-picture" alt="" src={imageData}></img> */}
-                    <input
-                    id="upload"
-                    type="file"
-                    name="image"
-                    accept="image/*"
-                    onChange={onFileChange}
-                    style={{ display: "none" }}
-                    />
-                </div>
+                        {/* <img className="upload-picture" alt="" src={imageData}></img> */}
+                        <input
+                        id="upload"
+                        type="file"
+                        name="image"
+                        accept="image/*"
+                        onChange={onFileChange}
+                        style={{ display: "none" }}
+                        />
+                    
+                    {!imageData && (
+                        // <div className="upload-image-container">
+                        <label
+                        htmlFor="upload"
+                        class="select-button"
+                    >
+                        <div class="select-button-text">写真をアップロードする</div>
+                        </label>
+                    )}
 
-                <label
-                    htmlFor="upload"
-                    class="upload-button"
-                >
-                    <div class="upload-button-text">写真をアップロードする</div>
-                </label>
-                
-                {/* {imageData && (
-                    <div>
-                        <img src={imageData} alt="Uploaded" width="200" height="60" />
-                    </div>
-                )} */}
+                    {imageData && (
+                        // <div className="upload-image-container">
+                        <img src={imageData} alt="Uploaded" className="upload-image" />
+
+                        // </div>
+                    )}
+
+                    <div className="upload-item-container">
+                    <label
+                        htmlFor="upload"
+                        class="upload-button"
+                    >
+                        <div class="upload-button-text">送信</div>
+                    </label>
+
+                    
+
+                    
+                  </div> 
                 </div>
               </div>
             </div>
           </div>
-        </div>
-    </div>
   );
 };
 
