26251a69902f6b3afee68996d5778056789f487e
e215723 <e215723@ie.u-ryukyu.ac.jp>
2024-01-14 20:52:57 +0900

    update(frontend): update header layout

--- frontend/src/pages/Upload.js
+++ frontend/src/pages/Upload.js
@@ -105,6 +105,7 @@ const Upload = () => {
                 Luck Bag
               </button>
             </div>
+            <img className="top-login-icon" src="./images/login-icon.png"></img>
           </header>
 
           <div
